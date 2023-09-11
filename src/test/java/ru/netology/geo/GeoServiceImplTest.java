e12b6af31ab966f0ce9f88b056b93b86734b3b24
TEOC <Foox210@yandex.ru>
2023-09-11 19:18:51 +0400

    First commit

---
+++ src/test/java/ru/netology/geo/GeoServiceImplTest.java
@@ -0,0 +1,29 @@
+package ru.netology.geo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+
+public class GeoServiceImplTest {
+    @Test
+    void tests_to_verify_location_by_ip() {
+        String ip = "96.0.0.0";
+        Location expected = new Location("New York", Country.USA, " 10th Avenue", 32);
+        GeoService geoService = new GeoServiceImpl();
+        Location preferences = geoService.byIp(ip);
+
+        Assertions.assertEquals(expected.getCountry(), preferences.getCountry());
+    }
+
+    @Test()
+    void testExpectedException() {
+
+        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
+            GeoServiceImpl exception = new GeoServiceImpl();
+            exception.byCoordinates(1, 1);
+        });
+
+        Assertions.assertEquals("Not implemented", thrown.getMessage());
+    }
+}
--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,3 +1,5 @@
+package ru.netology.sender;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -64,59 +66,6 @@ public class MessageSenderImplTest {
         Mockito.verify(geoService, Mockito.times(1))
                 .byIp(Mockito.<String>any());
 
-    }
-
-    @Test
-    void sends_text() {
-
-        GeoServiceImpl geoService = Mockito.spy(GeoServiceImpl.class);
-        LocalizationServiceImpl localizationService = Mockito.spy(LocalizationServiceImpl.class);
-        MessageSenderImpl messageSender = new MessageSenderImpl(geoService, localizationService);
-
-        HashMap mapRu = new HashMap();
-        mapRu.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.0.0.0");
-
-        HashMap mapENG = new HashMap();
-        mapENG.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.0.0.0");
-
-        String expectedENG = "Welcome";
-        String expectedRu = "Добро пожаловать";
-
-        String preferencesENG = messageSender.send(mapENG);
-        String preferencesRu = messageSender.send(mapRu);
-
-        Assertions.assertEquals(expectedRu, preferencesRu);
-        Assertions.assertEquals(expectedENG, preferencesENG);
-
-    }
-
-    @Test
-    void tests_to_verify_location_by_ip() {
-        String ip = "96.0.0.0";
-        Location expected = new Location("New York", Country.USA, " 10th Avenue", 32);
-        GeoService geoService = new GeoServiceImpl();
-        Location preferences = geoService.byIp(ip);
-
-        Assertions.assertEquals(expected.getCountry(), preferences.getCountry());
-    }
-
-    @Test
-    void checking_the_returned_text() {
-        String expected = "Добро пожаловать";
-        String preferences = new LocalizationServiceImpl().locale(Country.RUSSIA);
-
-        Assertions.assertEquals(expected, preferences);
-    }
-
-    @Test()
-    void testExpectedException() {
-
-        RuntimeException thrown = Assertions.assertThrows(RuntimeException.class, () -> {
-            GeoServiceImpl exception = new GeoServiceImpl();
-            exception.byCoordinates(1, 1);
-        });
-
-        Assertions.assertEquals("Not implemented", thrown.getMessage());
     }
 
 }
